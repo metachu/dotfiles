c94909ce7b9d56a1a7a62215d4259af809a756b4
William Chu <chudood@gmail.com>
2016-11-02 20:49:49 +1100

    commiting for new ubuntu 16.04 and adding bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+USERNAME=william
+#sudo apt-get update
+#sudo apt-get upgrade
+
+sudo apt-get install vim emacs i3-wm tree htop git fish stow feh i3 i3-status thefuck python-pip fonts-font-awesome gmrun
+
+echo "Enter password for use $USERNAME to change shell to fish:"
+chsh -s /usr/bin/fish
+
+rm ~/.bashrc
+
+cd ~/dotfiles
+stow laptop
+
+cd ~
+echo "Installing oh my fish"
+curl -L http://get.oh-my.fish | fish
+
+omf install robbyrussell
+
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
